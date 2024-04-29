66bd66bfaac479813aa9d2d54bc9fea6f7428a8f
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-04-29 17:52:18 -0300

    feat: WW-02 => Finalização do projeto com insersação dos dados adotando as boas praticas de desenvolvimento

--- src/app/components/weather/weather-card/weather-card.component.ts
+++ src/app/components/weather/weather-card/weather-card.component.ts
@@ -1,5 +1,12 @@
 import { WeatherDatas } from 'src/app/models/interfaces/Weather';
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  faDroplet,
+  faTemperature0,
+  faTemperatureHigh,
+  faTemperatureLow,
+  faWind,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-card',
@@ -8,10 +15,14 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class WeatherCardComponent implements OnInit {
   @Input() weatherDatasInput!: WeatherDatas;
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
 
   constructor() {}
 
   ngOnInit() {
-    console.log(this.weatherDatasInput, 'recebi')
+    console.log(this.weatherDatasInput, 'recebi');
   }
 }
